be155fbe540d6826fc41d8192665a62753fbfbf2
faeqf <qefqfqff@naver.com>
2020-07-04 10:27:50 +0900

    테스트

--- 2day/js/main.js
+++ 2day/js/main.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
-    //document 는 쉽게말해서 body 태그이다.
-    $(".green").css({ width: 200 }); //이렇게 하면 index.html에서 script가 위에 있어도 된다.
-}
-//$(".green").css({ width: 200 }); 
+  //$(".green").css({ width: 200, height: 200, backgorundColor: "ff33399" });
+  //귄장하는 사양은 아니다 권장하는것은 아래다.
+  $(".green").addClass("modify");
+});
+//document 는 쉽게말해서 body 태그이다.
+//이렇게 하면 index.html에서 script가 위에 있어도 된다.
+$("#red").css({ width: 300 });
+$(".black").css({ width: 200 });
+
+//$(".green").css({ width: 200 });
 //달러그린을 찾아서 css를 바꾸겠다.
 //or jQuery(".green") or document.queryselecton(".green");
