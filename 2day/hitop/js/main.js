1889021bc91172ab3f672bdbd427b7c7df7b229a
faeqf <qefqfqff@naver.com>
2020-07-04 12:25:51 +0900

    모바일버젼 실습

--- 2day/hitop/js/main.js
+++ 2day/hitop/js/main.js
@@ -1,4 +1,3 @@
-/*
 $("#gnb .list > li").on("mouseenter", function () {
   $(this).find(".deprh02").stop().slideDown(400);
 });
@@ -9,8 +8,8 @@ $("#gnb .list > li").on("mouseenter", function () {
 //그것을 방지하기 위해 stop() 을 적어 대기된 명령들을 다 해제하고 slide 한다.
 
 //jqury로 addeventlisner 가 on이다
-*/
 
+/*
 //모바일경우
 $("#gnb .list > li > a").on("click", function () {
   //내꺼 아닌놈들은 닫아야함
@@ -18,3 +17,4 @@ $("#gnb .list > li > a").on("click", function () {
   $(this).parent().find(".deprh02").stop().slideToggle(400); //parent,children 은 찾아가는 것, 토글은 상태에따라 up, down함
   return false; //모든 함수에서 리턴 밑은 암것도안됨
 });
+*/
