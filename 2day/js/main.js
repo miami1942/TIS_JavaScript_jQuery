b3f52c51ae18cd5121a30783beba2b12c1d5f110
faeqf <qefqfqff@naver.com>
2020-07-04 10:15:36 +0900

    ㅁㅁㅁ

---
+++ 2day/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    //document 는 쉽게말해서 body 태그이다.
+    $(".green").css({ width: 200 }); //이렇게 하면 index.html에서 script가 위에 있어도 된다.
+}
+//$(".green").css({ width: 200 }); 
+//달러그린을 찾아서 css를 바꾸겠다.
+//or jQuery(".green") or document.queryselecton(".green");
